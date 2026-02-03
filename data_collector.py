f4209749dcebe11ada1155fae1d236f1d4cf11ec
GOLLAPALLY SAI ARCHAN <154690460+Kushi1928@users.noreply.github.com>
2026-02-03 23:07:38 +0530

    Delete data_collector.py

--- data_collector.py
+++
@@ -1,7 +0,0 @@
-from langchain_core.tools import tool
-from tools.market_data_tool import fetch_company_data
-
-@tool
-def data_collector_agent(company: str):
-    """Collects market data and recent news for a company"""
-    return fetch_company_data(company)
